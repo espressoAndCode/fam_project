b7a61365c4569d15258ad91c0e722d1497c14981
Wes Bailey <espressoandcode@gmail.com>
2020-07-18 16:26:26 -0400

    Code cleanup in db_conn.py

--- fam/db_conn.py
+++ fam/db_conn.py
@@ -4,10 +4,8 @@ import datetime
 
 
 def create_data(record):
-    print(f"DB: {record}")
-    # return
-
-
+    # print(f"DB: {record}")
+ 
     # connecting to the database 
     cnx = mysql.connector.connect( 
                         host = os.getenv('FAM_HOST'), 
@@ -15,47 +13,14 @@ def create_data(record):
                         passwd = os.getenv('FAM_PASS'), 
                         database = os.getenv('FAM_DB') ) 
 
-
     cursor = cnx.cursor() 
-
-
-# need to figure out datetime format
-    # insert_stmt = ("INSERT INTO EVENTLOG VALUES ("datetime(record[0]), record[1], record[2], record[3], record[4], record[5]")")
-
     insert_stmt =  "INSERT INTO EVENTLOG (Timecode, Filepath, Syscall, Success, Exe, Auid) VALUES (%s, %s, %s, %s, %s, %s)"
     data_stmt = (record[0], record[1], record[2], record[3], record[4], record[5])
-
-    # query = "SELECT * FROM FILESTATE"
-
     cursor.execute(insert_stmt, data_stmt)
-
-    # res = cursorObject.fetchall()
-
-    # for item in res:
-    #     print(item)
-
-    # disconnecting from server 
-
 
     cnx.commit()
     cursor.close()
     cnx.close() 
-
-    # (
-    #     Timecode        DATETIME            NOT NULL,
-    #     Filepath        VARCHAR(255)        NOT NULL,
-    #     Syscall         VARCHAR(64)         NOT NULL,
-    #     Success         INT                 NOT NULL,
-    #     Exe             VARCHAR(255)        NOT NULL,
-    #     Auid            VARCHAR(64)         NOT NULL          
-    # );
-
-
-
-
-
-
-
 
 
 def read_data():
@@ -69,11 +34,8 @@ def read_data():
 
 
     cursor = cnx.cursor() 
-
     query = "SELECT * FROM FILESTATE"
-
     cursor.execute(query)
-
     res = cursor.fetchall()
 
     for item in res:
